66de5744a97d4f9f2903de2d12036f131822ef4b
Adam Gray <a.gray.4797@gmail.com>
2021-04-27 10:11:21 +0100

    Removed unneccesary imports, all now working

--- programs/evendigits.py
+++ programs/evendigits.py
@@ -1,4 +1,17 @@
-import programs.evencheck as evencheck
+def is_prime(x):
+    for n in range(2, (x//2)+1):
+        if x % n == 0:
+            return False
+            break
+        else:
+            pass
+    else:
+        return True
+
+
+def check_even(y):
+    primefacts = {i for i in range(2, y+1) if is_prime(i) == True and y % i == 0}
+    return y == 0 or 2 in primefacts
 
 
 def even_digits(x, y):
@@ -6,7 +19,7 @@ def even_digits(x, y):
     for num in range(x, y+1):
         strng = str(num)
         for d in strng:
-            if not evencheck.check_even(int(d)):
+            if not check_even(int(d)):
                 break
             else:
                 pass
--- tests/test_evendigits.py
+++ tests/test_evendigits.py
@@ -1,5 +1,20 @@
 import pytest
-from programs import evendigits as evendigits
+from programs import evendigits
+
+def test_is_prime():
+    assert evendigits.is_prime(2) == True
+    assert evendigits.is_prime(3) == True
+    assert evendigits.is_prime(4) == False
+    assert evendigits.is_prime(6) == False
+
+
+
+def test_check_even():
+    assert evendigits.check_even(0) == True
+    assert evendigits.check_even(2) == True
+    assert evendigits.check_even(3) == False
+    assert evendigits.check_even(5) == False
+
 
 def test_evendigits():
     assert evendigits.even_digits(10, 30) == "20,22,24,26,28,"
