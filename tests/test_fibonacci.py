d4ce607c40847da709692d6039230d3de077fd53
Adam Gray <a.gray.4797@gmail.com>
2021-05-05 08:40:19 +0100

    further updated pytest

--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -9,3 +9,4 @@ def test_fibonacci():
     assert fibonacci.fib(1.0) == 1
     assert fibonacci.fib('5') == 5
     assert fibonacci.fib(-3) == "Negative inputs not accepted!"
+    assert fibonacci.fib(True) == 1
